8697f6c7ab3f2e8f28d8a6b372c5a0f1d866e7f3
Ankit <136968673+AnkitKumarDelta@users.noreply.github.com>
2025-11-12 13:03:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-//your code here!
+const list = document.querySelector('ol');
 
+let count = 10;
+for (let i = 1; i <= count; i++) {
+  const li = document.createElement('li');
+  li.textContent = `Item ${i}`;
+  list.appendChild(li);
+}
+
+function addMoreItems() {
+  for (let i = 1; i <= 2; i++) {
+    count++;
+    const li = document.createElement('li');
+    li.textContent = `Item ${count}`;
+    list.appendChild(li);
+  }
+}
+
+list.addEventListener('scroll', () => {
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+    addMoreItems();
+  }
+});
